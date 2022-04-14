77bd383ee0d8cdda860c5419adcb233ddf3c370e
eyupyasuntimur <eyup.yasuntimur@appcent.mobi>
2022-04-14 19:40:43 +0300

    inittial commit

---
+++ KocSistemDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KocSistemDemo
+//
+//  Created by Eyüp on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
